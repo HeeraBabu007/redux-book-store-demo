83f57cebcc04a75d2472bc0627d8dcd2cb0ca038
Heera Babu <heerbabu@in.ibm.com>
2020-06-14 23:42:29 +0530

    logger middleware in Redux Part- 10

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import bookReducer from './book/bookReducer';
-
-const store =createStore(bookReducer);
+import logger from 'redux-logger';
+const store =createStore(bookReducer, applyMiddleware(logger));
 
 export default store;
