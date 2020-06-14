8a98b588cf13c6e2e6168b18f603b2c1e34a4f17
Heera Babu <heerbabu@in.ibm.com>
2020-06-14 23:22:36 +0530

    Use Redux with React Hooks | useSelector | useDispatch --9

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import BookContainer from './components/bookContainer';
+import HookBookContainer from './components/hookBookContainer';
 import store from './redux/store';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <BookContainer/>
+        <BookContainer />
+        <HookBookContainer />
       </div>
     </Provider>
   );
